36fc6b90762affdd7f6a9f110ee7e6d6d5a530d5
mdnaimur <mdnaimurr@gmail.com>
2020-02-29 21:53:40 +0600

    successfully loaded static resources with homepage

---
+++ onlineshopping/src/main/java/com/mnr/onlineshopping/PageController.java
@@ -0,0 +1,17 @@
+package com.mnr.onlineshopping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+         mv.addObject("starting","Bismillam to spring mvc");
+		return mv;
+	}
+
+}
